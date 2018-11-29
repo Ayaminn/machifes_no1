afbe1f76a751a62d9ea9adf76bfec954ae37a990
Ayaminn <ayamin.imai@gmail.com>
2018-11-29 20:47:07 +0900

    見つけた数が増えないのお〜〜〜

--- Assets/Scripts/TreasureNumScript.cs
+++ Assets/Scripts/TreasureNumScript.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TreasureNumScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public TreasureCountScript tcSctipt;
+    public GameObject unityChan;
+    private int treasure;
+    private Text treasureNum;
+
+    // Use this for initialization
+    void Start() {
+        treasureNum = GetComponentInChildren<Text>();
+        unityChan = GameObject.Find("UnityChan");
+        tcSctipt = unityChan.GetComponent<TreasureCountScript>();
+        treasure = tcSctipt.treasure;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        treasureNum.text = treasure.ToString();
+    }
 }
