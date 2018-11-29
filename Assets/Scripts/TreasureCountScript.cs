012137f059068459d139ae030ed6d6bce75a5db3
Ayaminn <ayamin.imai@gmail.com>
2018-11-29 19:13:07 +0900

    お宝に触れたら…というスクリプトをかいた

---
+++ Assets/Scripts/TreasureCountScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasureCountScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision col){
+
+        if(col.gameObject.tag == "Treasure")
+        {
+            Destroy(col.gameObject);
+            Debug.Log("おたからみーっけ！");
+        }
+
+    }
+}
